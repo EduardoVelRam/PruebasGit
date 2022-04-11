ec6cd188fdb9a81302d4ac35ec00a72269ad1b4f
EduardoVelascoR <69228747+EduardoVelascoR@users.noreply.github.com>
2022-04-11 11:21:14 -0500

    Primer proyecto web subido a Git

---
+++ tienda/admin.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+from .models import CategoriaProd, Producto
+
+# Register your models here.
+
+class CategroiaProdAdmin(admin.ModelAdmin):
+
+    readonly_fields = ("created", "updated")
+
+class ProductoAdmin(admin.ModelAdmin):
+
+    readonly_fields = ("created","updated")
+
+admin.site.register(CategoriaProd, CategroiaProdAdmin)
+
+admin.site.register(Producto, ProductoAdmin)
