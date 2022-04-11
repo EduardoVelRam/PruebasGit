a6c3e2d616508de68feea5d36f77e035849aedb9
EduardoVelascoR <69228747+EduardoVelascoR@users.noreply.github.com>
2022-04-11 11:27:33 -0500

    Primera modificación específica

--- ProyectoWeb/settings.py
+++ ProyectoWeb/settings.py
@@ -142,7 +142,7 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = "pwaderondo@gmail.com"
-EMAIL_HOST_PASSWORD = "MODIFICAR"
+EMAIL_HOST_USER = "correo@gmail.com"
+EMAIL_HOST_PASSWORD = "password"
 
 CRISPY_TEMPLATE_PACK = "bootstrap4"
