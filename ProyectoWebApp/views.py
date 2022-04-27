52f81455831364431b88370c4819fbe9b1a0f03e
EduardoVelascoR <69228747+EduardoVelascoR@users.noreply.github.com>
2022-04-27 14:04:53 -0500

    Ajustes al carro

--- ProyectoWebApp/views.py
+++ ProyectoWebApp/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render, HttpResponse
+from carro.carro import Carro
 from servicios.models import Servicio
 
 # Create your views here.
 
 def home(request):
+
+    carro = Carro(request)
 
     return render(request, "ProyectoWebApp/home.html")
     
--- carro/context_processor.py
+++ carro/context_processor.py
@@ -7,4 +7,8 @@ def importe_total_carro(request):
     
             for key, value in request.session["carro"].items():
                 total = total + float(value["precio"])
+
+    else:
+        total="Debes hacer login"
+
     return {"importe_total_carro":total}
